14df2dd09d267fb6c7724f520a5293ddfb6cf1b4
lokChingt <C24385243@myTUDublin.ie>
2025-11-08 22:58:11 +0000

    feat(auth): add register logic with username check

--- pages/register.php
+++ pages/register.php
@@ -1,30 +1,46 @@
 <?php 
 include "includes/header.php";
 include "includes/db_connect.php";
+include "includes/show_message.php";
 
 // Check if form is submitted
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $username       = $_POST['username'];
-    $fname          = $_POST['fname'];
-    $surname        = $_POST['surname'];
-    $address_line_1 = $_POST['address_line_1'];
-    $address_line_2 = $_POST['address_line_2'];
-    $city           = $_POST['city'];
-    $telephone      = $_POST['telephone'];
-    $mobile         = $_POST['mobile'];
-    $password       = $_POST['password'];
+    $username  = $_POST['username'];
+    $fname     = $_POST['fname'];
+    $surname   = $_POST['surname'];
+    $address_1 = $_POST['address_1'];
+    $address_2 = $_POST['address_2'];
+    $city      = $_POST['city'];
+    $telephone = $_POST['telephone'];
+    $mobile    = $_POST['mobile'];
+    $password  = $_POST['password'];
 
-    $sql = "INSERT INTO Users (Username, Password, FirstName, Surname, AddressLine1, AddressLine2, City, Telephone, Mobile) 
-    VALUES('$username', '$password', '$fname', '$surname', '$address_line_1', '$address_line_2', '$city', '$telephone', '$mobile')";
 
-    if ($conn->query($sql) === TRUE) {
-        $conn->close();
-        header("Location: login.php");
-        exit();
+    // check if username exist
+    $checkUserStmt = $conn -> prepare("SELECT Username FROM Users WHERE Username = ?;");
+    $checkUserStmt -> bind_param("s", $username);
+    $checkUserStmt -> execute();
+    $result = $checkUserStmt -> get_result();
+
+    // username been used
+    if($result && $result -> num_rows > 0) {
+        $message = "Username already been used";
     } else {
-        $error = "Error: " . $conn->error;
-        $conn->close();
+        // username not been used
+        $stmt = $conn -> prepare("INSERT INTO Users VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)");
+        $stmt -> bind_param("sssssssss", $username, $password, $fname, $surname, $address_1, $address_2, $city, $telephone, $mobile);
+        
+        if($stmt -> execute()) {
+            # redirect to login page
+            header("Location: login.php");
+            exit();
+        } else {
+            $message = "Error: " . $stmt->error;
+        }
+        $stmt -> close();
     }
+    $checkUserStmt -> close();
+    $conn -> close();
 }
 
 ?>
@@ -33,68 +49,43 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <form method="POST">
     <table>
         <tr>
-            <label>
-                <td>First Name:</td>
-                <td><input type="text" name="fname" placeholder="First Name" required></td>
-            </label>
+            <td><label for="fname">First Name:</label></td>
+            <td><input type="text" id="fname" name="fname" placeholder="First Name" required></td>
         </tr>
         <tr>
-            <label>
-                <td>Surname:</td>
-                <td><input type="text" name="surname" placeholder="Surname" required></td>
-            </label>
+            <td><label for="surname">Surname:</label></td>
+            <td><input type="text" id="surname" name="surname" placeholder="Surname" required></td>
         </tr>
         <tr>
-            <label>
-                <td>Address Line 1:</td>
-                <td><input type="text" name="address_line_1" placeholder="Address Line 1"></td>
-            </label>
+            <td><label for="address_1">Address Line 1:</label></td>
+            <td><input type="text" id="address_1" name="address_1" placeholder="Address Line 1"></td>
         </tr>
         <tr>
-            <label>
-                <td>Address Line 2:</td>
-                <td><input type="text" name="address_line_2" placeholder="Address Line 2"></td>
-            </label>
+            <td><label for="address_2">Address Line 2:</label></td>
+            <td><input type="text" id="address_2" name="address_2" placeholder="Address Line 2"></td>
         </tr>
         <tr>
-            <label>
-                <td>City:</td>
-                <td><input type="text" name="city" placeholder="City" required></td>
-            </label>
+            <td><label for="city">City:</label></td>
+            <td><input type="text" id="city" name="city" placeholder="City" required></td>
         </tr>
         <tr>
-            <label> 
-                <td>Telephone:</td>
-                <td><input type="text" name="telephone" placeholder="Telephone"></td>
-            </label>
+            <td><label for="telephone">Telephone:</label></td>
+            <td><input type="text" id="telephone" name="telephone" placeholder="Telephone"></td>
         </tr>
         <tr>
-            <label>
-                <td>Mobile:</td>
-                <td><input type="text" name="mobile" placeholder="Mobile"></td>
-            </label>
+            <td><label for="mobile">Mobile:</label></td>
+            <td><input type="text" id="mobile" name="mobile" placeholder="Mobile"></td>
         </tr>
         <tr>
-            <label>
-                <td>Username:</td>
-                <td><input type="text" name="username" placeholder="Username" required></td>
-            </label>
+            <td><label for="username"> Username:</label></td>
+            <td><input type="text" id="username" name="username" placeholder="Username" autocomplete="username" required></td>
         </tr>
         <tr>
-            <label>
-                <td>Password:</td>
-                <td><input type="text" name="password" placeholder="Password" required></td>
-            </label>
+            <td><label for="password"> Password:</label></td>
+            <td><input type="password" id="password" name="password" placeholder="Password" required></td>
         </tr>
     </table>
     <input type="submit" value="Register">
 </form>
 
-<?php 
-// Display error if any
-if (isset($error)) {
-    echo "<div class='error'>" . $error . "</div>";
-}
-
-include "includes/footer.php";
-?>
+<?php include "includes/footer.php";?>
