1a9aa93ceed39bfa753a716eced4d2dad9baf9e7
lokChingt <C24385243@myTUDublin.ie>
2025-11-08 23:09:56 +0000

    style(ui): update display layout

--- pages/index.php
+++ pages/index.php
@@ -13,35 +13,18 @@ $result = $conn->query($sql);
 
 <h1>This is the Home page</h1>
 
-<p>All Books</p>
-<table>
-    <tr>
-        <th>ISBN</th>
-        <th>Book Title</th>
-        <th>Author</th>
-        <th>Edition</th>
-        <th>Year</th>
-        <th>Category</th>
-        <th>Status</th>
-    </tr>
-    <?php 
-        // Show all books
-        if ($result->num_rows > 0) {
-            while($row = $result->fetch_assoc()) {
-                echo "<tr>";
-                    echo "<td>" . $row['ISBN'] . "</td>";
-                    echo "<td>" . $row['BookTitle'] . "</td>";
-                    echo "<td>" . $row['Author'] . "</td>";
-                    echo "<td>" . $row['Edition'] . "</td>";
-                    echo "<td>" . $row['Year'] . "</td>";
-                    echo "<td>" . $row['CategoryDetail'] . "</td>";
-                    echo "<td>" . $row['Status'] . "</td>";
-                echo "</tr>";
-            }
-        }
-        $conn->close();
-    ?>
-</table>
+<h2>All Books</h2>
+<?php 
+    // show all books with BookTitle, Edition and Author
+    while ($row = $result -> fetch_assoc()) {
+        echo "<div class='book'>";
+        echo '<img src="../images/template.png" alt="Book Template" height="200">' . '<br>';
+        echo "<a href='book_info.php?isbn={$row['ISBN']}'> {$row['BookTitle']} [Edition {$row['Edition']}] </a> <br>";
+        echo "{$row['Author']}</p>";
+        echo "</div>";
+    }
+    $conn->close();
+?>
 
 
 <?php include "includes/footer.php";?>
