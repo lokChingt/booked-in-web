17abcdf711eae0fd534adc9a3912e1e72c629165
lokChingt <C24385243@myTUDublin.ie>
2025-11-18 19:45:35 +0000

    feat(auth): unset user ID on logout and reduce refresh time

--- pages/logout.php
+++ pages/logout.php
@@ -1,9 +1,10 @@
 <?php 
 session_start();
+unset($_SESSION['userid']);
 unset($_SESSION['username']);
 include "includes/header.php";
 $error = "You have logged out";
 include "includes/show_message.php";
-header('Refresh: 1; URL = ../pages/index.php');
+header('Refresh: 0.5; URL = ../pages/index.php');
 exit();
 ?>
